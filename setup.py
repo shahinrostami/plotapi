46cc28e173df6090fd5d4e7728cec27de7054d7f
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-09-06 15:04:12 +0100

    chore: Update package with deps

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="plotapi",
-    version="6.1.1",
+    version="6.1.2",
     description="Engaging visualisations, made easy.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -14,5 +14,6 @@ setup(
     author_email="hello@plotapi.com",
     license="MIT",
     packages=["plotapi"],
+    install_requires=["requests >=2.20.0, <3.0.0"],
     zip_safe=False,
 )
